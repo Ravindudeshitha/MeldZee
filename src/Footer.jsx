f803e8ba064714243a60a6e090bc7b1aeebedbe1
Ravindu Deshitha <ravindudeshitha01@gmail.com>
2025-03-08 09:59:30 +0530

    responsive

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaArrowUp } from "react-icons/fa"; // Importing up arrow icon
 
 const Footer = () => {
   // Function to scroll to the top
@@ -10,15 +11,15 @@ const Footer = () => {
   };
 
   return (
-    <footer className="bg-gray-800 text-white py-12"> {/* Increased height */}
-      <div className="container mx-auto flex justify-between items-center">
+    <footer className="bg-gray-800 text-white py-12 relative"> {/* Increased height */}
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row justify-between items-center space-y-6 sm:space-y-0">
         {/* MeldZee Logo */}
-        <div className="text-4xl font-bold text-left"> {/* Increased font size */}
+        <div className="text-4xl font-bold text-left">
           <span className="text-blue-500">Meld</span>Zee
         </div>
 
         {/* Footer Links */}
-        <div className="flex space-x-6">
+        <div className="flex flex-wrap justify-center sm:justify-start space-x-6">
           <a href="#about" className="hover:text-blue-500">About</a>
           <a href="#projects" className="hover:text-blue-500">Products</a>
           <a href="#contact" className="hover:text-blue-500">Contact</a>
@@ -28,18 +29,18 @@ const Footer = () => {
         </div>
 
         {/* Contact Number */}
-        <div className="text-lg">
+        <div className="text-lg text-center sm:text-left">
           <p>Contact: 0719188465</p>
         </div>
       </div>
 
       {/* Go Up Button */}
-      <div className="flex justify-center mt-6">
+      <div className="absolute -top-12 right-6 flex justify-center">
         <button
           onClick={handleGoUp}
-          className="bg-blue-500 text-white px-6 py-2 rounded-full hover:bg-blue-600 transition"
+          className="bg-blue-500 text-white rounded-full p-3 hover:bg-blue-600 transition flex items-center justify-center"
         >
-          Go Up
+          <FaArrowUp size={20} /> {/* Using the up arrow icon */}
         </button>
       </div>
     </footer>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,27 +1,33 @@
-import image1 from './assets/home.jpg'
+import image1 from './assets/home.jpg';
 
 const Home = () => {
-    return (
-      <section id="home" className="h-[80vh] relative">
-        {/* Background Image */}
-        <img 
-          src={image1} 
-          alt="Home Background" 
-          className="absolute inset-0 w-full h-full object-cover"
-        />
-  
-        {/* Buttons (Positioned at Bottom Right) */}
-        <div className="absolute bottom-6 right-6 flex space-x-4">
-          <a href="#projects" className="border-2 w-28 border-gray-700 text-center text-gray-700 font-bold px-4 py-2 rounded shadow hover:bg-gray-200">
-            Projects
-          </a>
-          <a href="https://blog.meldzee.com/" target="_blank"  className="border-2 w-28 border-gray-500 text-center text-gray-500 font-bold px-4 py-2 rounded shadow hover:bg-gray-200">
-            Blog
-          </a>
-        </div>
-      </section>
-    );
-  };
-  
-  export default Home;
-  
+  return (
+    <section id="home" className="relative w-full">
+      {/* Background Image */}
+      <img
+        src={image1}
+        alt="Home Background"
+        className="w-full h-auto sm:h-[40vh] md:h-[50vh] lg:h-[70vh] object-cover pt-16 sm:pt-0"
+      />
+
+      {/* Buttons (Positioned at Bottom Right) */}
+      <div className="absolute bottom-6 sm:bottom-8 right-6 sm:right-8 flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
+        <a
+          href="#projects"
+          className="border-2 w-24 sm:w-28 border-gray-700 text-center text-gray-700 font-bold px-4 py-2 rounded shadow hover:bg-gray-200"
+        >
+          Projects
+        </a>
+        <a
+          href="https://blog.meldzee.com/"
+          target="_blank"
+          className="border-2 w-24 sm:w-28 border-gray-500 text-center text-gray-500 font-bold px-4 py-2 rounded shadow hover:bg-gray-200"
+        >
+          Blog
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
